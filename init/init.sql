245e2195a74573f74801a42aac4445e418c0cbd1
berkeaziz <berke-aziz2001@hotmail.com>
2026-04-25 23:46:08 +0300

    Add initial SQL schema and table definitions for raw trades

---
+++ init/init.sql
@@ -0,0 +1,17 @@
+CREATE SCHEMA IF NOT EXISTS raw;
+CREATE SCHEMA IF NOT EXISTS staging;
+CREATE SCHEMA IF NOT EXISTS marts;
+
+CREATE TABLE IF NOT EXISTS raw.trades (
+    id BIGSERIAL PRIMARY KEY,
+    event_type VARCHAR(50),
+    symbol VARCHAR(20) NOT NULL,
+    trade_id BIGINT,
+    price NUMERIC(18,8) NOT NULL,
+    quantity NUMERIC(18,8) NOT NULL,
+    trade_time TIMESTAMPTZ NOT NULL,
+    is_buyer_maker BOOLEAN,
+    source VARCHAR(50),
+    ingested_at TIMESTAMPTZ NOT NULL,
+    event_time TIMESTAMP NOT NULL
+);
