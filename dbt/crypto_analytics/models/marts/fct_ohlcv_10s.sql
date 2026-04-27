5f0bc7d519166fc49aea45b8e5d42c1b8261559f
berkeaziz <berke-aziz2001@hotmail.com>
2026-04-27 23:21:02 +0300

    Refactor fct_ohlcv_10s model SQL for consistency and clarity

--- dbt/crypto_analytics/models/marts/fct_ohlcv_10s.sql
+++ dbt/crypto_analytics/models/marts/fct_ohlcv_10s.sql
@@ -1,6 +1,6 @@
 {{ config(
-    materialized = "table",
-    schema = "marts"
+    materialized='table',
+    schema='marts'
 ) }}
 
 SELECT
@@ -21,8 +21,21 @@ SELECT
 
     CASE
         WHEN close_price > open_price THEN 'bullish'
-        ELSE 'bearish'
+        WHEN close_price < open_price THEN 'bearish'
+        ELSE 'neutral'
     END AS candle_type,
+
+    CASE
+        WHEN open_price IS NOT NULL AND open_price != 0
+        THEN ((close_price - open_price) / open_price) * 100
+        ELSE NULL
+    END AS candle_return_pct,
+
+    CASE
+        WHEN volume IS NOT NULL AND trade_count IS NOT NULL AND trade_count != 0
+        THEN volume / trade_count
+        ELSE NULL
+    END AS avg_trade_size,
 
     created_at
 
