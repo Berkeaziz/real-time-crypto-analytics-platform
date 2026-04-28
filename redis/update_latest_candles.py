14f42ce285e2a8e003933811dc45a9ff2d972f89
berkeaziz <berke-aziz2001@hotmail.com>
2026-04-28 16:02:40 +0300

    Refactor update_latest_candles function to include error handling and refresh interval

--- redis/update_latest_candles.py
+++ redis/update_latest_candles.py
@@ -1,5 +1,6 @@
 import json
 import os
+import time
 from decimal import Decimal
 from datetime import datetime
 
@@ -16,6 +17,8 @@ POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "crypto_pass")
 REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
 REDIS_PORT = int(os.getenv("REDIS_PORT", "6379"))
 
+REFRESH_SECONDS = int(os.getenv("REFRESH_SECONDS","5"))
+
 
 def json_default(obj):
     if isinstance(obj, Decimal):
@@ -24,19 +27,18 @@ def json_default(obj):
         return obj.isoformat()
     return str(obj)
 
-
-def main():
+def update_latest_candles():
     pg_conn = psycopg2.connect(
-        host=POSTGRES_HOST,
-        port=POSTGRES_PORT,
-        dbname=POSTGRES_DB,
-        user=POSTGRES_USER,
-        password=POSTGRES_PASSWORD,
+        host = POSTGRES_HOST,
+        port =POSTGRES_PORT,
+        dbname =POSTGRES_DB,
+        user = POSTGRES_USER,
+        password =POSTGRES_PASSWORD,
     )
 
     r = redis.Redis(
-        host=REDIS_HOST,
-        port=REDIS_PORT,
+        host =REDIS_HOST,
+        port = REDIS_PORT,
         decode_responses=True,
     )
 
@@ -57,24 +59,34 @@ def main():
             created_at
         FROM public_marts.fct_ohlcv_10s
         ORDER BY symbol, window_start DESC;
-    """
-
+    """    
     with pg_conn.cursor() as cur:
         cur.execute(query)
-        cols = [desc[0] for desc in cur.description]
+        cols =[desc[0] for desc in cur.description]
         rows = cur.fetchall()
-
+        
     for row in rows:
-        data = dict(zip(cols, row))
+        data=dict(zip(cols,row))
         symbol = data["symbol"]
 
-        key = f"latest_candle:{symbol}"
-        r.set(key, json.dumps(data, default=json_default))
-
-        print(f"Updated Redis key: {key}")
-
+        key =f"latest_candle:{symbol}"
+        r.set(key,json.dumps(data,default=json_default))
+        
+        print(f"Updated Redis key:{key}")
+    
     pg_conn.close()
 
+
+def main():
+    print("Redis writer started..")
+
+    while True:
+        try:
+            update_latest_candles()
+        except Exception as e:
+            print(f"Redis writer error: {e}")
+        
+        time.sleep(REFRESH_SECONDS)
 
 if __name__ == "__main__":
     main()
